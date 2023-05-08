5cb44c0d99039b3b85819ccd3932b80ca35ccb80
lesinai <90961075+lesinai@users.noreply.github.com>
2023-05-08 22:18:29 +0200

    wtf

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,10 @@
-// Add imports above this line
-import SimpleLightbox from "simplelightbox"
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+
+
+      
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,3 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-// const player = new Player('handstick', {
-//     id: 19231868,
-//     width: 640
-// });
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
