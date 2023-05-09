ade1e599d37db23a7382917dc31bce93eb546c7f
lesinai <90961075+lesinai@users.noreply.github.com>
2023-05-09 10:18:14 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,6 +4,33 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 
+const galleryRef = document.querySelector('.gallery');
+
+const imagesToPaste = galleryItems.map(item => {
+    const { preview, original, description } = item;
+
+  return  `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+          loading="lazy"
+        />
+      </a>
+    </li>`
+      
+}
+)
+galleryRef.insertAdjacentHTML("beforeend", imagesToPaste.join(''))
+const option = {
+  captionsData: 'alt',
+captionDelay: 250,
+}
+
+new SimpleLightbox('.gallery a', option);
+
+
 
       
 
