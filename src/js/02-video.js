9a89dd4dbba43f94b1d9737c8b052053d671d43b
lesinai <90961075+lesinai@users.noreply.github.com>
2023-05-09 10:29:22 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const videoRef = document.querySelector('#vimeo-player')
+const player = new Player(videoRef);
+
+if (localStorage.getItem("videoplayer-current-time")) {  
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+}
+
+const onPlay = function (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}; 
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
