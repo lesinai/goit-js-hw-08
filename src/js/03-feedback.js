8d3b5539c0025d053a87edd4e56e934aba554183
lesinai <90961075+lesinai@users.noreply.github.com>
2023-05-09 10:49:09 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
-import throttle  from'lodash.throttle';
+import throttle from 'lodash.throttle';
+
+
+const formRef = document.querySelector('.feedback-form');
+
+if (localStorage.getItem("feedback-form-state")) {
+    const savedInputs = storeService.load("feedback-form-state")
+     for (const input in savedInputs) {
+        if (savedInputs[input]) {
+            formRef[input].value = savedInputs[input];
+        }
+    }
+}
+
+
+formRef.addEventListener('input', throttle(onInputForm, 500))
+formRef.addEventListener('submit', onSubmitForm)
+
+
+let inputHolder = {};
+
+function onInputForm(e) {
+    inputHolder[e.target.name] = e.target.value;
+    storeService.save("feedback-form-state", inputHolder);
+}
+
+function onSubmitForm(e) {
+    e.preventDefault();
+    localStorage.removeItem("feedback-form-state");
+    formRef.reset();
+}
